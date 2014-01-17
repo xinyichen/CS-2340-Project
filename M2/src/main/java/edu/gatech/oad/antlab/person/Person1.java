449c464de48e25880cfb808e61d2d9b43738e33c
xinyichen <chenxinyicxy@hotmail.com>
2014-01-17 04:44:56 -0500

    Xinyi: implemented method in Person1.java

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -30,8 +30,12 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+	  if (input == null) return null;
+	  
+	  if (input.length() <= 2)
+	      return input;
+	  else
+	      return input.substring(2) + input.substring(0, 2);
 	}
 	
 	/**
