27e47da080d22c96a49e209c41111f3a8f4373f6
xinyichen <chenxinyicxy@hotmail.com>
2014-01-27 20:08:52 -0500

    M3

---
+++ M3/src/com/example/myfirstapp/Starting.java
@@ -0,0 +1,44 @@
+package com.example.myfirstapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+public class Starting extends Activity {
+
+	MediaPlayer ourSong;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.starting);
+		ourSong =MediaPlayer.create(Starting.this, R.raw.startingsound);
+		ourSong.start();
+		Thread timer = new Thread(){
+			public void run(){
+				try{
+					sleep(4000);
+				}
+				catch(InterruptedException e){
+					e.printStackTrace();
+				}
+				finally{
+					Intent openMain = new Intent("com.example.myfirstapp.MENU");
+					startActivity(openMain);
+				}
+			}
+		};
+		timer.start();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		ourSong.release();
+		finish();
+	}
+
+}
