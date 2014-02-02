3bcef5c3e212e5cbdc8de5291a637da8155419ce
Zhuoming Li(Ming) <zhuoming.li01@gmail.com>
2014-02-02 16:33:51 -0500

    WhereIsMyMoney

---
+++ WhereIsMyMoney/src/com/example/whereismymoney/Welcome.java
@@ -0,0 +1,73 @@
+package com.example.whereismymoney;
+
+import com.example.myfirstapp.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Welcome extends Activity {
+	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+	
+	Button login, createNewAccount;
+	Bundle a;
+	
+	
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		this.onCreate(a);
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		a = savedInstanceState;
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_welcome);
+		login = (Button) findViewById(R.id.bLogin);
+		createNewAccount = (Button) findViewById(R.id.bCreateNewAccount);
+		
+		login.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				Intent goToLogin = new Intent("com.example.myfirstapp.MAINACTIVITY");
+				startActivity(goToLogin);
+			}
+		});
+		
+		createNewAccount.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+			
+		
+		//viewPager
+		//Log.d(TAG,"activity creating");
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public void sendMessage(View view) {
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		EditText editText = (EditText) findViewById(R.id.edit_userID);
+		String message = editText.getText().toString();
+		intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent);
+	}
+
+}
