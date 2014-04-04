a5a86f0e156f279ed4f9633a54ec40bcfa3fa179
Andrew Farrow <afarrow3@gatech.edu>
2014-04-03 21:50:53 -0400

    Did application side of password hashing
    Application side of hashing is basically complete (still haven’t done
    the server side part)

--- WhereIsMyMoney/src/com/whereismymoney/model/PasswordManager.java
+++ WhereIsMyMoney/src/com/whereismymoney/model/PasswordManager.java
@@ -3,29 +3,44 @@ package com.whereismymoney.model;
 import org.jsoup.nodes.Document;
 
 import com.whereismymoney.database.DatabaseConnect;
+import com.whereismymoney.service.PasswordHash;
 
 /**
  * This class handles everything associated with logins and user registration.
  */
 
 public class PasswordManager {
+    private PasswordHash hasher;
+
+    public PasswordManager() {
+        hasher = new PasswordHash();
+    }
 
     /**
      * Checks to see if the login is valid.
      * 
      * @param username username for login 
      * @param password password for login
+     *
      * @return Returns true if the login is valid, false otherwise.
      */
     public boolean login(String username, String password) {
+        //TODO: get the hash from the server
+
+
         Document doc = DatabaseConnect.getDatabaseConnect().login(username,
                 password);
+
+
         String loginResult = (doc.text());
-        if (loginResult.equals("Found")) {
+        if(loginResult.equals("Not found")) {
+            return false;
+        } else if(hasher.validatePassword(password, loginResult)) {
             CurrentUser.getCurrentUser().setUserName(username);
             return true;
+        } else {
+            return false
         }
-        return false;
     }
 
     /**
@@ -36,19 +51,22 @@ public class PasswordManager {
      * @param last_name new user last name
      * @param password new user password
      * @param email new user email address
+     *
      * @return True if the user was created, false otherwise
      */
 
     public boolean register(String username, String first_name,
             String last_name, String password, String email) {
-        Document doc = DatabaseConnect.getDatabaseConnect().register(username,
-                first_name, last_name, password, email);
-        String loginResult = (doc.text());
-        if (loginResult.equals("registered")) {
+        
+        String hashedPassword = hasher.createHash(password);
+
+        Document doc = DatabaseConnect.getDatabaseConnect().hashedRegister(username,
+                first_name, last_name, hashedPassword, email);
+        String registerResult = (doc.text());
+        if (registerResult.equals("registered")) {
             return true;
         }
 
         return false;
     }
-
 }
